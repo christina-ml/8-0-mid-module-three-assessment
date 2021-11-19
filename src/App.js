fa7b0242847e4a9746b203f6a9b2cc645dd1f537
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-19 11:05:04 -0500

    setup cart and set up products

--- src/App.js
+++ src/App.js
@@ -13,9 +13,38 @@ class App extends Component{
   render(){
     return(
       <div>
-        <h1>My Garage Sale</h1>
-        <div className="products">Products</div>
-        <div className="cart">Cart</div>
+        <h2>My Garage Sale</h2>
+        {/* START - products section */}
+
+        <div className="products">
+          <h3>Products</h3>
+          <div className="product-card">
+            <div>Price: $Price.00</div>
+            <br />
+            <button type="submit">Add To Cart</button>
+            <br />
+            <img src="product-img" alt="product image" />
+            <br />
+            <div>Product Description</div>
+          </div>
+        </div>
+
+        {/* END - products section */}
+        {/* START - cart section */}
+
+        <div className="cart">
+          <h3>Cart</h3>
+          <ul>
+            <li>Item Name: $Price.00</li>
+            <li>Item Name: $Price.00</li>
+          </ul>
+          <div><h3>Subtotal: $Price.00</h3></div>
+          <div><h3>Tax: $Price.00</h3></div>
+          <div><h3>Total: $Price.00</h3></div>
+        </div>
+
+        {/* END - cart section */}
+        {/* START - checkout section */}
         <div className="form" id="checkout">
           <h3>Checkout</h3>
           <form>
@@ -44,6 +73,7 @@ class App extends Component{
             </div>
           </form>
         </div>
+        {/* END - checkout section */}
       </div>
     )
   }
