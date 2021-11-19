2d1aa4549fef3881ff025c998e7bf21446041fb3
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-19 10:52:41 -0500

    setup checkout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,49 @@ import { Component } from "react";
 import "./App.css";
 
 class App extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+
+    }
+  }
+
   render(){
     return(
-      <h1>My Garage Sale</h1>
+      <div>
+        <h1>My Garage Sale</h1>
+        <div className="products">Products</div>
+        <div className="cart">Cart</div>
+        <div className="form" id="checkout">
+          <h3>Checkout</h3>
+          <form>
+            <div>
+              <label htmlFor="first-name">First Name</label><br />
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="last-name">Last Name</label><br />
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label><br />
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="credit-card">Credit Card</label><br />
+              <input type="text" />
+            </div>
+            <div>
+              <label htmlFor="zip-code">Zip Code</label><br />
+              <input type="text" />
+            </div>
+            <div>
+              <button type="submit">Buy Now</button>
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 }
